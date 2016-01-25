8126b3b67cf5942122baf7f1b8feb58601cc4d3b
Zhu Wu <zhu.wu@outlook.com>
2016-01-26 02:04:22 +0800

    add header and footer navigation

--- blogapp/app/helpers/application_helper.rb
+++ blogapp/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def navigation_menu is_top
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+    presenter.css = is_top ? 'nav' : 'nav bottom-nav'
+    presenter.list_tag_css= 'menu'
+    presenter.dom_id = ''
+    presenter.selected_css = 'current'
+    presenter.active_css = 'current'
+    presenter.first_css = ''
+    presenter.last_css = ''
+    presenter
+  end
 end
