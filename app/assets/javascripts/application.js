ca8a7c8981d36ef010d8b68974e4c9f2c27cc418
Zhu Wu <zhu.wu@outlook.com>
2016-02-24 11:43:41 +0800

    add sharing buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,23 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+(function() {
+  var initAdthis = function(){
+    // Remove all global properties set by addthis, otherwise it won't reinitialize
+    for (var i in window) {
+        if (/^addthis/.test(i) || /^_at/.test(i)) {
+            delete window[i];
+        }
+    }
+    window.addthis_share = null;
+
+    // Finally, load addthis
+    $.getScript("//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50b5b67007d0b4d4");
+  }
+
+  // Trigger the function on both jquery's ready event and turbolinks page:change event
+  $(document).on('ready page:change', function() {
+      initAdthis();
+  });
+}) ();
