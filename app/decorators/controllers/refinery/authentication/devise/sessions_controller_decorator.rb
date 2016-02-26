e6395f414beb1d1737190d8a01081ff829fdc433
Zhu Wu <zhu.wu@outlook.com>
2016-02-27 07:16:36 +0800

    add otp for backend login

--- app/decorators/controllers/refinery/authentication/devise/sessions_controller_decorator.rb
+++ app/decorators/controllers/refinery/authentication/devise/sessions_controller_decorator.rb
@@ -1,3 +1,25 @@
+require 'rotp'
+
 Refinery::Authentication::Devise::SessionsController.class_eval do
-  
+  before_action :check_otp, :only => [:create]
+
+  protected
+
+  def check_otp
+    name = params[:authentication_devise_user][:login]
+    otp = params[:otp]
+    userId = Refinery::Authentication::Devise::User.where(["username = ? OR email = ?", name, name]).order(:id).limit(1).pluck(:id)[0];
+    raise RuntimeError if (otp.blank? || userId.blank?)
+
+    secret = Token.where(["refinery_authentication_devise_user_id = ?", userId]).order(:id).limit(1).pluck(:secret)[0];
+    raise RuntimeError if secret.blank?
+
+    totp = ROTP::TOTP.new(secret, {:interval => 60})
+    raise RuntimeError if !totp.verify(otp)
+
+  rescue RuntimeError
+    flash[:error] = t('wrong_username_otp')
+    logger.error("Wrong username/email or wrong OTP was entered. Username: [#{name}]. OTP: [#{otp}].")
+    redirect_to :back
+  end
 end
