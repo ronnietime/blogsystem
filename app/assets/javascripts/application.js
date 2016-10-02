f173e5c5e25aa8dfe85ad6fad92778fba009617e
Zhu Wu <zhu.wu@outlook.com>
2016-10-03 00:03:49 +0800

    improved infinite scroll behavior

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,85 +14,115 @@
 //= require jquery_ujs
 //= require_tree .
 
-(function() {
-  var toggleMenu = function() {
-    $('.menu-toggle').on('click', function(e) {
-      e.preventDefault();
-      var target = $(this),
-          menu = $('.slide-menu'),
-          body = $('body');
-      if (target.hasClass('toggle-on')) {
-        target.removeClass('toggle-on');
-        menu.removeClass('expanded');
-        body.removeClass('sidebar-open');
-      } else {
-        target.addClass('toggle-on');
-        menu.addClass('expanded');
-        body.addClass('sidebar-open');
+function toggleMenu() {
+  $('.menu-toggle').on('click', function(e) {
+    e.preventDefault();
+    var target = $(this),
+        menu = $('.slide-menu'),
+        body = $('body');
+    if (target.hasClass('toggle-on')) {
+      target.removeClass('toggle-on');
+      menu.removeClass('expanded');
+      body.removeClass('sidebar-open');
+    } else {
+      target.addClass('toggle-on');
+      menu.addClass('expanded');
+      body.addClass('sidebar-open');
+    }
+  });
+}
+
+// Current page starts from 1
+function initScroll(currentPage, totalPages) {
+  function buildUrl(pageIndex) {
+    return location.pathname + (pageIndex == 1 ? '' : '?page=' + pageIndex);
+  }
+
+  function infiniteScroll() {
+    var currentWindowTop = $(window).scrollTop();
+
+    if (currentWindowTop > windowTop) {
+      // Scroll down
+      if (currentPage < totalPages && currentWindowTop + $(window).height() > pageTops[currentPage + 1]) {
+        currentPage++;
+        $(window).off('scroll', infiniteScroll);
+        if ($('div[data-page=' + currentPage + ']').length === 0) {
+          $('#posts').append(window.pageHtmls[currentPage]);
+          pageTops = getTops();
+        }
+        history.pushState({}, '', buildUrl(currentPage));
+        if (currentPage < totalPages && !window.pageHtmls[currentPage + 1]) {
+          $.getScript(buildUrl(currentPage + 1), function () {
+            $(window).on('scroll', infiniteScroll);
+          });
+        } else {
+          $(window).on('scroll', infiniteScroll);
+        }
+      }
+    }
+
+    if (currentWindowTop < windowTop) {
+      // Scroll up
+      if (currentPage >= 2 && currentWindowTop < pageTops[currentPage]) {
+        currentPage--;
+        $(window).off('scroll', infiniteScroll);
+        if ($('div[data-page=' + currentPage + ']').length === 0) {
+          $('#posts').prepend(window.pageHtmls[currentPage]);
+          pageTops = getTops();
+          $(document).scrollTop(pageTops[currentPage + 1] - pageTopOffset);
+        }
+        history.pushState({}, "", buildUrl(currentPage));
+        if (currentPage >= 2 && !window.pageHtmls[currentPage - 1]) {
+          $.getScript(buildUrl(currentPage - 1), function () {
+            $(window).on('scroll', infiniteScroll);
+          })
+        } else {
+          $(window).on('scroll', infiniteScroll);
+          $('#additional-top').remove();
+        }
+      }
+    }
+
+    windowTop = currentWindowTop;
+  }
+
+  function getTops() {
+    var pages = $('div[data-page]'),
+        positions = [];
+
+    pages.each(function(index) {
+      var top = $(this).position().top - pageTopOffset - headerHeight,
+          pageIndex = parseInt($(this).data('page'));
+      positions[pageIndex] = top;
+      if (index === pages.length - 1) {
+        positions[pageIndex + 1] = top + $(this).height();
       }
     });
-  };
 
-  var initScroll = function() {
-    function infiniteScroll() {
-      var currentPosition = $(window).scrollTop();
+    return positions;
+  }
 
-      if (currentPosition > position) {
-        if (pagesInfo.nextPageUrl && $(window).scrollTop() + $(window).height() > page_positions[page_positions.length - 1].bottom) {
-          $(window).off('scroll', infiniteScroll);
-          $.getScript(pagesInfo.nextPageUrl, function() {
-            updateState(pagesInfo.nextPageUrl);
-          });
-        }
-      } else {
-        if (pagesInfo.prevPageIndex >= 1 && $(window).scrollTop() < page_positions[0].top) {
-          var prevPageUrl = location.pathname + '?page=' + pagesInfo.prevPageIndex;
-          $(window).off('scroll', infiniteScroll);
-          $.getScript(prevPageUrl + '&direction=up', function() {
-            updateState(pagesInfo.prevPageIndex == 1 ? location.pathname : prevPageUrl);
-            $(document).scrollTop(page_positions[1].top);
-          });
-        }
-      }
+  window.pageHtmls = [];
+  var windowTop = $(window).scrollTop(),
+      headerHeight = $('.site-header').outerHeight(),
+      pageTopOffset = parseInt($('.post').eq(0).css('margin-top')),
+      pageTops = getTops();
 
-      position = currentPosition;
-    }
+  // Prefetch previous page and next page
+  if (currentPage > 1) {
+    $.getScript(buildUrl(currentPage - 1));
+  }
+  if (currentPage < totalPages) {
+    $.getScript(buildUrl(currentPage + 1));
+  }
 
-    function getPagesInfo() {
-      var pages = $('div[data-page]'),
-          firstPage = pages.first(),
-          positions = [];
-
-      pages.each(function() {
-        var top = $(this).position().top,
-            height = $(this).height();
-        positions.push({top: top, bottom: top + height})
-      });
-
-      return {
-        prevPageIndex: firstPage.data('page') - 1,
-        nextPageUrl: $('.pagination .next_page').prop('href'),
-        positions: positions
-      };
-    }
-
-    function updateState(url) {
-      history.pushState({}, '', url);
-      pagesInfo = getPagesInfo();
-      page_positions = pagesInfo.positions;
-      $(window).on('scroll', infiniteScroll);
-    }
-
-    var position = $(window).scrollTop(),
-        pagesInfo = getPagesInfo(),
-        page_positions = pagesInfo.positions;
-
+  if ($('body').hasClass('listing')) {
     $(window).on('scroll', infiniteScroll);
-  };
+  }
+}
 
-  $(document).ready(function() {
-    toggleMenu();
-    Prism.highlightAll();
-    initScroll();
-  });
-}) ();
+$(document).ready(function() {
+  toggleMenu();
+  Prism.highlightAll();
+  window.scrollTo(0, $('#additional-top').height() || 0);
+});
