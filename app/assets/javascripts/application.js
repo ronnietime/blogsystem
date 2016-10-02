3e8133afd33a130b09f61a0794de552d5e9fa91f
Zhu Wu <zhu.wu@outlook.com>
2016-10-02 09:11:49 +0800

    an improved version of infinite scroll, but not perfect yet.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,22 +31,68 @@
         body.addClass('sidebar-open');
       }
     });
-  }
+  };
 
-  var infiniteScroll = function() {
-    if ($('#infinite-scrolling').length > 0) {
-      $(window).on('scroll', function() {
-        var nextPageUrl = $('.pagination .next_page').prop('href');
-        if (nextPageUrl && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
-          $.getScript(nextPageUrl);
+  var initScroll = function() {
+    function infiniteScroll() {
+      var currentPosition = $(window).scrollTop();
+
+      if (currentPosition > position) {
+        if (pagesInfo.nextPageUrl && $(window).scrollTop() + $(window).height() > page_positions[page_positions.length - 1].bottom) {
+          $(window).off('scroll', infiniteScroll);
+          $.getScript(pagesInfo.nextPageUrl, function() {
+            updateState(pagesInfo.nextPageUrl);
+          });
         }
-      });
+      } else {
+        if (pagesInfo.prevPageIndex >= 1 && $(window).scrollTop() < page_positions[0].top) {
+          var prevPageUrl = location.pathname + '?page=' + pagesInfo.prevPageIndex;
+          $(window).off('scroll', infiniteScroll);
+          $.getScript(prevPageUrl + '&direction=up', function() {
+            updateState(pagesInfo.prevPageIndex == 1 ? location.pathname : prevPageUrl);
+            $(document).scrollTop(page_positions[1].top);
+          });
+        }
+      }
+
+      position = currentPosition;
     }
-  }
+
+    function getPagesInfo() {
+      var pages = $('div[data-page]'),
+          firstPage = pages.first(),
+          positions = [];
+
+      pages.each(function() {
+        var top = $(this).position().top,
+            height = $(this).height();
+        positions.push({top: top, bottom: top + height})
+      });
+
+      return {
+        prevPageIndex: firstPage.data('page') - 1,
+        nextPageUrl: $('.pagination .next_page').prop('href'),
+        positions: positions
+      };
+    }
+
+    function updateState(url) {
+      history.pushState({}, '', url);
+      pagesInfo = getPagesInfo();
+      page_positions = pagesInfo.positions;
+      $(window).on('scroll', infiniteScroll);
+    }
+
+    var position = $(window).scrollTop(),
+        pagesInfo = getPagesInfo(),
+        page_positions = pagesInfo.positions;
+
+    $(window).on('scroll', infiniteScroll);
+  };
 
   $(document).ready(function() {
     toggleMenu();
     Prism.highlightAll();
-    infiniteScroll();
+    initScroll();
   });
 }) ();
